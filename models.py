fe998b901e6c2b03aaff57606e67614a43ee386f
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-10 23:53:43 +0900

    Update models.py

--- models.py
+++ models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(
+        max_length = 15, 
+        unique = True,
+        null = True,
+        error_messages = {'unique':'중복된 nickname 입니다.'})
