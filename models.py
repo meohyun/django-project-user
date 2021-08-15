32fb3b4281616f0e4eef998e11a01b0b9e2d99db
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-15 22:42:44 +0900

    Update models.py

--- models.py
+++ models.py
@@ -4,17 +4,43 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     nickname = models.CharField(
-        max_length = 15, 
-        unique = True,
+        max_length=15,
+        unique=True,
+        null=True,
+        error_messages={"unique":"닉네임이 중복입니다."},)
+    
+    student_number = models.IntegerField(
         null = True,
-        error_messages = {'unique':'중복된 nickname 입니다.'})
-    kakao_id = models.CharField(
-        max_length = 20,
-        null = True,
-        validators = [validate_no_special_characters])
-    # override
+        unique= True,
+        error_messages={"unique":"학번이 중복입니다."},
+        validators=[validate_student_number]
+    )
+
     username = models.CharField(
-        max_length = 25,
-        null = True,
-        unique = False,
-        validators = [validate_no_special_characters])
+        max_length= 30,
+        null= True,
+        unique= False,
+        validators=[validate_no_special_characters]
+    )  
+
+    def __str__(self):
+        return self.email
+
+
+class Post(models.Model):
+    class_name = models.CharField(max_length=20, validators=[validate_no_special_characters])
+    professor_name = models.CharField(max_length=10,validators=[validate_no_special_characters])
+    GRADE_CHOICE =[
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+    ]
+    grade = models.IntegerField(choices=GRADE_CHOICE)
+    file = models.FileField(upload_to='zokbo')
+    content = models.TextField()
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.class_name
