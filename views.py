5e1fe333dcb039d277b45c20ea0b8855b2c2b2ca
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-16 22:33:29 +0900

    Update views.py

--- views.py
+++ views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'trade/index.html')
+
+class DetailZokboView(DetailView):
+    model = Post
+    template_name = 'trade/detail.html'
+    
+
+class CreateZokboView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'trade/form.html'
+
+    def get_success_url(self):
+        return reverse('detail',kwargs={'pk':self.object.id})
