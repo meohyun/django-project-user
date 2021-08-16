b7835097eefae2f3e60243b903fe8f96d18fc365
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-16 22:36:04 +0900

    Update models.py

--- models.py
+++ models.py
@@ -42,5 +42,6 @@ class Post(models.Model):
     dt_created = models.DateTimeField(auto_now_add=True)
     dt_updated = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User,on_delete=models.CASCADE )
     def __str__(self):
         return self.class_name
