9dd840cf6a9801b64a1be994b337e1599771d328
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-16 22:32:39 +0900

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User,Post
 
 class UserForm(forms.ModelForm):
   class Meta:
@@ -10,3 +10,17 @@ class UserForm(forms.ModelForm):
       user.nickname = self.cleaned_data["nickname"]
       user.student_number = self.cleaned_data["student_number"]
       user.save()
+      
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ["class_name","professor_name","grade","file",'content']
+
+    def create_post(self,request,post):
+      post.class_name = self.class_name['class_name']
+      post.professor_name = self.class_name['professor_name']
+      post.grade = self.class_name['grade']
+      post.file = self.class_name['file']
+      post.content = self.class_name['content']
+            
+    
