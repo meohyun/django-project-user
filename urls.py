d7e5c24314d0be312758b4a7bca4362605db7fcf
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-16 22:33:05 +0900

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('/zokbo/<int:pk>/',views.DetailZokboView.as_view(),name='detail'),
+    path('/zokbo_upoload/',views. CreateZokboView.as_view(),name ='create')
 ]
