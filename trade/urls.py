bd6780c4a83649ac128cbdd47ab1976a0213c0ba
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-17 22:44:54 +0900

    Update urls.py

--- trade/urls.py
+++ trade/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
-    path('/zokbo/<int:pk>/',views.DetailZokboView.as_view(),name='detail'),
-    path('/zokbo_upoload/',views. CreateZokboView.as_view(),name ='create')
+    path('zokbo/<int:pk>/',views.DetailZokboView.as_view(),name='detail'),
+    path('zokbo_upoload/',views.CreateZokboView.as_view(),name ='create'),
+    path('zokbo/<int:pk>/update,views.UpdateZokboView.as_view(),name='update'),
+    path('zokbo/<int:pk>/delete,views.DeleteZokboView.as_view(),name='delete')     
+     
 ]
