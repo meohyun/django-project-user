3be83bacf15691ffc42cc5887f7ab58ba0dac4c4
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-19 19:33:38 +0900

    Update urls.py

--- trade/urls.py
+++ trade/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
+    path('email_required/',views.TemplateView.as_view(template_name='account/email_confirmation_required.html'),name='required'),
+    path('email_confirmation/'views.TemplateView.as_view(template_name='account/email_confirmation_done.html'),name='done'),
     path('zokbo/<int:pk>/',views.DetailZokboView.as_view(),name='detail'),
     path('zokbo_upoload/',views.CreateZokboView.as_view(),name ='create'),
     path('zokbo/<int:pk>/update,views.UpdateZokboView.as_view(),name='update'),
