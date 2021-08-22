eeb9e21adc2b9db7dc12f423f96b067ecd620d44
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-22 22:35:49 +0900

    Update urls.py

--- trade/urls.py
+++ trade/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('zokbo/<int:pk>/',views.DetailZokboView.as_view(),name='detail'),
     path('zokbo_upoload/',views.CreateZokboView.as_view(),name ='create'),
     path('zokbo/<int:pk>/update,views.UpdateZokboView.as_view(),name='update'),
-    path('zokbo/<int:pk>/delete,views.DeleteZokboView.as_view(),name='delete')     
-     
+    path('zokbo/<int:pk>/delete,views.DeleteZokboView.as_view(),name='delete'),
+    path('profile/<int:user_id>/',views.ProfileView.as_view(),name= 'profile'),
+    path('profile/<int:user_id>/reviews/',views.UserReviewListView.as_view(),name='user-review-list'),    
 ]
