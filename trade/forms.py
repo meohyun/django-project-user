a941d870f3fbb99a6a2884b5351ca63321d9e0d6
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-23 17:11:29 +0900

    Update forms.py

--- trade/forms.py
+++ trade/forms.py
@@ -2,16 +2,6 @@ from django import forms
 from .models import User,Post
 
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["nickname",'student_number']
-
-        def signup(self,request,User):
-            User.nickname = self.cleaned_data['nickname']
-            User.student_number = self.cleaned_data['student_number']
-            User.save()
-
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
