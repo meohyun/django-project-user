bfee46db5c28c3f0238f4e9b5a313af46ad88d2f
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-23 17:17:34 +0900

    Create middleware.py

---
+++ trade/middleware.py
@@ -0,0 +1,25 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+      if (
+          request.user.is_authenticated and
+          not request.user.nickname and
+          request.path_info != reverse('profile-set')
+       ):
+        return redirect('profile-set')
+       
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
