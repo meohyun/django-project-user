0733478ae67c6d96e87e1f79c23a21d4acac6dc2
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-23 17:19:00 +0900

    Update middleware.py

--- trade/middleware.py
+++ trade/middleware.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
-class SimpleMiddleware:
+class ProfileSetupMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
