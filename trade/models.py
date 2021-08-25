e89232694d8fe6e7a656ef36a5f1af1c443610f7
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-25 17:20:56 +0900

    Update models.py

--- trade/models.py
+++ trade/models.py
@@ -1,6 +1,9 @@
 from trade.validators import validate_no_special_characters,validate_student_number
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.urls import reverse
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -23,12 +26,12 @@ class User(AbstractUser):
         unique= False,
         validators=[validate_no_special_characters]
     ) 
-    
+
     profile_pic = models.ImageField(
         upload_to = 'profile_pics',
-        default = 'default_profile_pic.jpg'   
+        default = 'default_profile_pic.jpg'
     )
-    
+
     intro = models.CharField(
         max_length = 60,
         blank = True
@@ -42,19 +45,42 @@ class Post(models.Model):
     class_name = models.CharField(max_length=20, validators=[validate_no_special_characters])
     professor_name = models.CharField(max_length=10,validators=[validate_no_special_characters])
     GRADE_CHOICE =[
-        (1,★),
-        (2,★★),
-        (3,★★★),
-        (4,★★★★),
-        (5,★★★★★),
+        (1,'★'),
+        (2,'★★'),
+        (3,'★★★'),
+        (4,'★★★★'),
+        (5,'★★★★★'),
     ]
-    grade = models.IntegerField(choices=GRADE_CHOICE)
-    upload_files = models.FileField(upload_to='zokbo')
-    content = models.TextField()
+    grade = models.IntegerField(choices=GRADE_CHOICE,default=None)
+    
+    content = models.CharField(max_length=50)
     dt_created = models.DateTimeField(auto_now_add=True)
     dt_updated = models.DateTimeField(auto_now=True)
-
     author = models.ForeignKey(User,on_delete=models.CASCADE )
+    upload_files = models.FileField(upload_to='zokbo')
+    
+
 
     def __str__(self):
         return self.class_name
+
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    dt_commented = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post,on_delete= models.CASCADE)
+    author = models.ForeignKey(User,on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        ordering = ['-id']
+    
+    def get_edit_url(self):
+        return reverse('comment-update',args=[self.post.pk, self.pk])
+
+    def get_delete_url(self):
+        return reverse('comment-delete',args=[self.post.pk, self.pk])
