b35f2aeb2c6ca272e8c4d6fdcb9d87d5f59b90bc
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-25 17:20:43 +0900

    Update urls.py

--- trade/urls.py
+++ trade/urls.py
@@ -3,19 +3,33 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
-    # email_confiramtion
-    path('email_required/',views.TemplateView.as_view(template_name='account/email_confirmation_required.html'),name='required'),
-    path('email_confirmation/'views.TemplateView.as_view(template_name='account/email_confirmation_done.html'),name='done'),
-    
-    # zokbo
+    path(
+        'email_confirmation/',
+        views.TemplateView.as_view(template_name='account/email_confirmation_required.html'),
+        name = 'required'),
+    path(
+        'email_confirmation_done/',
+        views.TemplateView.as_view(template_name='account/email_confirmation_done.html'),
+        name = 'confirmation_done'),
     path('zokbo/<int:pk>/',views.DetailZokboView.as_view(),name='detail'),
     path('zokbo_upoload/',views.CreateZokboView.as_view(),name ='create'),
-    path('zokbo/<int:pk>/update,views.UpdateZokboView.as_view(),name='update'),
-    path('zokbo/<int:pk>/delete,views.DeleteZokboView.as_view(),name='delete'),
-         
+    path('zokbo/<int:pk>/update',views.UpdateZokboView.as_view(),name ='update'),
+    path('zokbo/<int:pk>/delete',views.DeleteZokboView.as_view(),name ='delete'),
+    path('zokbo/<int:pk>/download',views.download,name='download'),
+    path('zokbo-list/',views.ListZokboView.as_view(),name='list'),
+
     # profile
     path('profile/<int:user_id>/',views.ProfileView.as_view(),name= 'profile'),
     path('profile/<int:user_id>/reviews/',views.UserReviewListView.as_view(),name='user-review-list'),
     path('profile-set/',views.ProfileSetView.as_view(),name= 'profile-set'),
-    path('edit-profilet',views.ProfileUpdateView.as_view(),name='profile-update')
+    path('edit-profilet',views.ProfileUpdateView.as_view(),name='profile-update'),
+
+    # search
+    path('search/',views.search,name='search'),
+
+    # comment
+    path('comments/<int:post_pk>',views.comment_new,name='comment-new'),
+    path('comments/<int:post_pk>/edit/<int:pk>',views.comment_update,name='comment-update'),
+    path('comments/<int:post_pk>/delete/<int:pk>',views.comment_delete,name='comment-delete'),
 ]
+
