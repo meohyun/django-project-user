d8466d7fb52bc30dc58b897b249e545e1544fb03
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-08-25 17:21:23 +0900

    Update admin.py

--- trade/admin.py
+++ trade/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Post
+from .models import User,Post,Comment
 
 # Register your models here.
 admin.site.register(User,UserAdmin)
-UserAdmin.fieldsets += ('Custom fields',{"fields":("nickname","student_number")}),)
+UserAdmin.fieldsets += (("Custom fields",{"fields":("nickname","student_number",'profile_pic','intro')}),)
 admin.site.register(Post)
+admin.site.register(Comment)
