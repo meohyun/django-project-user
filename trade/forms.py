a30d18b232c94da69d8f2f709392f1e2510229c7
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-09-17 17:33:00 +0900

    Update forms.py

--- trade/forms.py
+++ trade/forms.py
@@ -5,10 +5,10 @@ from .models import User,Post,Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["class_name","professor_name","grade","upload_files",'content']
+        fields = ["class_name","professor_name","grade","upload_files",'content',"grade_num","semester"]
         widgets = {
-            'grade' : forms.RadioSelect
-            
+            'grade' : forms.RadioSelect,
+
         }
 
 class ProfileForm(forms.ModelForm):
